7a06526de1fbb7c5ca66056364efc1ada59ba65d
Vikas kumar <vikaskumarmca1985@gmail.com>
2022-01-29 21:06:27 +0530

    Update Laravel_command.php

--- Laravel_command.php
+++ Laravel_command.php
@@ -19,6 +19,12 @@ php artisan make:migration create_admins_table
 
 php artisan optimize:clear   (clear cache)
 
+migrate:fresh         Drop all tables and re-run all migrations
+migrate:install       Create the migration repository
+migrate:refresh       Reset and re-run all migrations
+migrate:reset         Rollback all database migrations
+migrate:rollback      Rollback the last database migration
+migrate:status        Show the status of each migration
 
 php artisan migrate 
 
