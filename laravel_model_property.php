b8b5371519ea371f8c721a5b88ed3c03ad436c05
Vikas kumar <vikaskumarmca1985@gmail.com>
2023-01-26 13:04:02 +0530

    Create laravel_model_property.php

---
+++ laravel_model_property.php
@@ -0,0 +1,16 @@
+ 
+
+This property is an array of fields that are mass assignable. 
+This means that these fields can be mass-assigned using the create or update method.
+
+
+protected $table ='my_students';
+protected $guarded = ['id'];
+protected $fillable = ['product_name','product_image'];
+protected $primaryKey ='student_id'
+$public $incrementing = false
+public $timestamps = false
+const CREATED_AT ='creation_date';
+const UPDATED_AT ='updation_date';
+
+protected = $attributes = ['name'=>'setDefaultValue'];
